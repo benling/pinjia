3fcd7116b53adebf776d97803ad851f5f97e8d25
lei <1521054657@qq.com>
2018-05-26 12:20:20 +0800

    first commit

---
+++ src/main/java/com/tsw/pinjia/PinjiaApplication.java
@@ -0,0 +1,12 @@
+package com.tsw.pinjia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinjiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinjiaApplication.class, args);
+    }
+}
